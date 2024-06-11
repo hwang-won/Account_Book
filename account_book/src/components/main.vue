7485e6c1d221d957537bef33ad0b2c102dba582a
g0mg0m <g0mg0m@pukyong.ac.kr>
2024-06-11 18:22:25 +0900

    feat: Add test  main.vue

---
+++ account_book/src/components/main.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <h1>로그인 성공시 메인페이지</h1>
+        <button @click="logout">(임시)로그아웃</button>
+
+
+    </div>
+</template>
+
+<script>
+export default {
+
+    methods: {
+
+
+        logout() {
+            localStorage.removeItem('loginKey'); 
+            console.log('로그아웃되었습니다.');
+            // 로그아웃 -> 로그인페이지로 이동
+            this.$router.push('/');
+        }
+    }
+}
+</script>
+
+<style lang="">
+</style>
