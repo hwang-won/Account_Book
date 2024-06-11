7d5d0b16255ab66f9578d6b3ebfb4308d1f0b32f
g0mg0m <g0mg0m@pukyong.ac.kr>
2024-06-11 13:36:09 +0900

    feature:Add components

---
+++ account_book/src/components/login.vue
@@ -0,0 +1,90 @@
+<template>
+    <div>
+        <h1>로그인</h1>
+        <br />
+        <!-- login화면 -->
+        <table border="1">
+            <tr>
+                <td>ID</td>
+                <td>
+                    <input v-model="id" placeholder="id" size="20" />
+                </td>
+            </tr>
+            <tr>
+                <td>PW</td>
+                <td>
+                    <input type="password" v-model="pw" placeholder="pw" size="20" />
+                </td>
+            </tr>
+            <tr>
+                <td colspan="2">
+                    <button v-on:click="login">로그인</button>
+
+                </td>
+            </tr>
+            <tr>
+                <td colspan="2">
+                    <a :href="'/findpw'">비밀번호 찾기  |</a>
+                    <a :href="'/regi'">  회원 가입</a>
+                </td>
+            </tr>
+        </table>
+    </div>
+
+</template>
+<script>
+import axios from 'axios';
+
+export default {
+    name: 'login',
+    data() {
+        return {
+            id: '',
+            pw: '',
+            login_data: {}
+        }
+    },
+    methods: {
+        login() {
+                // ID와 PW가 빈칸인지 검사
+                if (!this.id || !this.pw) {
+                    alert("ID와 PW를 모두 입력해주세요.");
+                    return;
+                }
+
+            // 사용자 정보 가져오기
+            axios.get(`http://localhost:3001/User?user_id=${this.id}&password=${this.pw}`)
+                .then(res => {
+
+                    if (res.data.length === 0) {
+                        alert('로그인 정보가 일치하지 않습니다.');
+                        return;
+                    }
+
+                    const user = res.data[0];
+                    if (user.user_id === this.id && user.password === this.pw) {
+                        this.login_data = { user_id: user.user_id, password: user.password };
+                        alert(user.user_id + "님 환영합니다");
+
+                        localStorage.setItem("login", JSON.stringify(this.login_data));
+
+                        // 메인페이지로 이동(추가)
+                        // this.$router.push('/bbslist');
+
+                        // 입력폼 초기화
+                        
+                    } else {
+                        alert('로그인 정보가 일치하지 않습니다.');
+                    }
+                })
+                .catch(err => alert('오류가 발생했습니다: ' + err));
+
+        }
+
+    }
+
+}
+</script>
+
+
+<style></style>
