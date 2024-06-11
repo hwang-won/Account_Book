b4d262a477a5b92604f313f38f1eefe45cd3660a
g0mg0m <g0mg0m@pukyong.ac.kr>
2024-06-11 17:33:43 +0900

    feat:Add routes id,pw_search

--- account_book/src/routes/user.js
+++ account_book/src/routes/user.js
@@ -1,7 +1,11 @@
 import loginView from '@/views/loginView.vue';
-import regi from '@/components/regi.vue';
-
 import mainView from '@/views/mainView.vue';
+
+import regi from '@/components/regi.vue';
+import id_search from '@/components/id_search.vue';
+import pw_search from '@/components/pw_search.vue';
+
+
 
 export default[
 
@@ -21,6 +25,18 @@ export default[
         component :regi
 
     },
+    {
+        path: '/id_search',
+        name: 'id_search',
+        component :id_search
+
+    },
+    {
+        path: '/pw_search',
+        name: 'pw_search',
+        component :pw_search
+
+    }
     
 
     
