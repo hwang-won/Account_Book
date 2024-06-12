54f077f8d1d32f94f5599f6a6fcc7d3d885a6926
hwang-won <w16831111@gmail.com>
2024-06-12 17:18:33 +0900

    feature: Add CSS

--- account_book/src/components/login/pw_search.vue
+++ account_book/src/components/login/pw_search.vue
@@ -1,45 +1,37 @@
 <template lang="">
-    <div>
+    <div class="loginBox">
         <h1>비밀번호 찾기</h1>
         <br>
         <!--입력 폼 -->
-        <table border="1">
+        <table border="1" class="loginTable">
             <tr>
-                <td>ID</td>
                 <td>
-                    <input v-model="id" placeholder="id" size="20" />
+                    <input type="text" v-model="id" placeholder="아이디" size="20" />
                 </td>
             </tr>
             <tr>
-                <td>이름</td>
                 <td>
-                    <input v-model="name" placeholder="name" size="20" />
+                    <input type="text" v-model="name" placeholder="이름" size="20" />
                 </td>
             </tr>
             <tr>
-                <td>이메일</td>
                 <td>
-                    <input v-model="email" placeholder="email" size="20" />
+                    <input type="text" v-model="email" placeholder="이메일" size="20" />
                 </td>
             </tr>
             
             <tr>
                 <td colspan="2">
-                    <button v-on:click="findPassword">PW찾기</button>
+                    <button v-on:click="findPassword" class="loginBtn">확인</button>
                 </td>
             </tr>
             <tr>
                 <td colspan="2">
-                    <a :href="'/id_search'"> id 찾기 |  </a>
-                    <a :href="'/'"> 로그인 페이지로 </a>
-
+                    <a :href="'/id_search'">아이디 찾기</a>
+                    <a :href="'/'">로그인</a>
                 </td>
             </tr>
-
         </table>
-        <br>
-        회원님의 password: {{ resultPassword }}
-
     </div>
 </template>
 <script>
@@ -70,8 +62,8 @@ export default {
                     const serverData = res.data[0];
                     if (serverData.user_id === this.id && serverData.name === this.name && serverData.email === this.email) {
                         this.resultPassword = serverData.password
-                        alert("password 찾기 성공");
-                        console.log("회원님의 Password :",this.resultPassword);
+                        alert("비밀번호 찾기 성공! 회원님의 비밀번호는 : " + this.resultPassword + " 입니다" );
+                        // console.log("회원님의 Password :",this.resultPassword);
                         //alert(serverData.user_id + "님 환영합니다");
 
                         // this.email = '';
@@ -82,12 +74,59 @@ export default {
                     }
                 })
                 .catch(err => alert('오류가 발생했습니다: ' + err));
-
-        }
+        },
     }
 }
 
 </script>
-<style lang="">
-
+<style scoped>
+h1 {
+    padding-top: 60px;
+}
+.loginBox   {
+    width: 700px;
+    height: 500px;
+    background-color: rgb(255, 245, 198);
+    border-radius: 50px;
+}
+.loginTable {
+    width: 100%;
+    height: 250px;
+    border: 1px solid rgb(255, 245, 198);
+}
+table {
+    width: 100%;
+    border-collapse: collapse;
+}
+th, td {
+    border: 1px solid rgb(255, 245, 198);
+    padding: 8px;
+}
+input[type=text] {
+    width: 400px;
+    padding: 12px;
+    font-size: 16px;
+    border-radius: 5px;
+    box-sizing: border-box;
+    border: 1px solid rgb(173, 173, 173);
+}
+input[type=text]:focus {
+    outline: none;
+    border: 1px solid black;
+}
+.loginBtn {
+    width: 400px;
+    border-radius: 5px;
+    background-color: rgb(255, 204, 0);
+    font-size: 20px;
+}
+td a {
+    font-size: 16px;
+    color: rgb(146, 146, 146);
+    padding: 10px;
+}
+td a:hover {
+    font-weight: 600;
+    color: rgb(0, 0, 0);
+}
 </style>
