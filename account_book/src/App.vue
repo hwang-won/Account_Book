80c1731b7251ebaa0d2f8d0515224a7ec9fa912f
JaesungGo <beeniy@naver.com>
2024-06-12 21:55:02 +0900

    feature: Add addToggle

--- account_book/src/App.vue
+++ account_book/src/App.vue
@@ -2,15 +2,17 @@
   <div id="app">
     <Header/>
       <router-view/>
+        <Toggle/>
   </div>
 </template>
 
 <script>
 import mainView from '@/views/mainView.vue';
 import Header from './components/header.vue';
+import Toggle from './components/body/addToggle.vue';
 export default {
   name : "App",
-  components : {mainView,Header}
+  components : {mainView,Header,Toggle}
 }
 </script>
 <style>
