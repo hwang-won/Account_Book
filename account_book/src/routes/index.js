262bc6302f404f5f08970b0587c03c9279998347
g0mg0m <g0mg0m@pukyong.ac.kr>
2024-06-12 15:41:50 +0900

    feat: edit routes

--- account_book/src/routes/index.js
+++ account_book/src/routes/index.js
@@ -1,14 +1,52 @@
 //라우터를 설정해줄 파일 
+import { createWebHistory, createRouter } from "vue-router";
 
-import { createWebHistory, createRouter} from "vue-router";
+import loginView from '@/views/loginView.vue';
+import regi from '@/components/regi.vue';
+import id_search from '@/components/id_search.vue';
+import pw_search from '@/components/pw_search.vue';
 
-import user from "@/routes/user.js";
+import mainView from '@/views/mainView.vue';
+import ProfileEdit from "@/components/profileEdit.vue";
+
+const routes = [
+
+    {
+        path: '/',
+        name: 'login',
+        component: loginView
+    },
+    {
+        path: '/main',
+        name: 'main',
+        component: mainView
+    },
+    {
+        path: '/regi',
+        name: 'regi',
+        component: regi
+
+    },
+    {
+        path: '/id_search',
+        name: 'id_search',
+        component: id_search
+    },
+    {
+        path: '/pw_search',
+        name: 'pw_search',
+        component: pw_search
+    },
+    {
+        path: '/profileEdit',
+        name: 'profileEdit',
+        component: ProfileEdit
+    }
+]
 
 const router = createRouter({
     history: createWebHistory(),
-    routes:[
-        ...user
-    ]
+    routes
 });
 
 export default router;
