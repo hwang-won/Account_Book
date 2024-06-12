c328faf141f4a402296de6c9f01cea75c2dc341a
g0mg0m <g0mg0m@pukyong.ac.kr>
2024-06-12 09:33:57 +0900

    feat: loginView

--- account_book/src/views/loginView.vue
+++ account_book/src/views/loginView.vue
@@ -1,1 +1,19 @@
-{\rtf1}
+<template>
+    <div>
+        <login/>
+    </div>
+</template>
+<script>
+import login from "@/components/login.vue";
+
+export default {
+    name:"loginView",
+    components:{
+        login
+    }
+    
+}
+</script>
+<style>
+    
+</style>
