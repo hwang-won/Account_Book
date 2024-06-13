3c5de2e4729271bb46f62c968d3deb915b51d70c
JaesungGo <102339979+JaesungGo@users.noreply.github.com>
2024-06-13 14:54:08 +0900

    Update: graph.vue

--- account_book/src/components/body/graph.vue
+++ account_book/src/components/body/graph.vue
@@ -1,15 +1,15 @@
 <template>
-    <div class="graph-container">
-      <div class="chart-item">
-        <PlusChart v-if="user" />
-      </div>
-      <div class="chart-item">
-        <MinusChart v-if="user" />
-      </div>
-      <div class="chart-item">
-        <SumChart v-if="user" />
-      </div>
+  <div class="graph-container">
+    <div class="chart-item">
+      <PlusChart v-if="user" />
     </div>
+    <div class="chart-item">
+      <MinusChart v-if="user" />
+    </div>
+    <div class="chart-item">
+      <SumChart v-if="user" />
+    </div>
+  </div>
 </template>
 <script>
 import PlusChart from "@/components/body/chart/plusChart.vue";
@@ -17,50 +17,101 @@ import MinusChart from "@/components/body/chart/minusChart.vue";
 import SumChart from "@/components/body/chart/sumChart.vue";
 
 export default {
-components: {
+  components: {
     MinusChart,
     PlusChart,
     SumChart,
-},
-data() {
+  },
+  data() {
     return {
-    user: null,
+      user: null,
     };
-},
-mounted() {
+  },
+  mounted() {
     const loginKey = localStorage.getItem("loginKey");
     if (!loginKey) {
-    this.$router.push("/");
+      this.$router.push("/");
     } else {
-    this.user = JSON.parse(loginKey);
+      this.user = JSON.parse(loginKey);
     }
-},
+  },
 };
 </script>
 
 <style scoped>
-    h2 {
-    text-align: center;
-    }
+.graph-container {
+  display: flex;
+  flex-wrap: wrap;
+  gap: 50px;
+  justify-content: center;
+  width: 1600px;
+}
 
-    .graph-container {
-    display: flex;
-    grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
-    gap: 20px;
-    flex: 1;
-    justify-items: center;
-    align-items: center;
-    transition: margin-left 0.3s;
-    }
+.chart-item {
+  background-color: #fff;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 30%;
+  height: 300px; 
+  transition: all 0.3s ease;
+}
 
-    .chart-item {
-    width: 100%;
-    height: 100%;
-    background-color: #fff;
-    box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
-    display: flex;
-    justify-content: center;
+@media (max-width: 1500px) {
+  .graph-container {
+    flex-direction: column;
     align-items: center;
-    transition: all 0.3s ease;
-    }
+    margin-top: 30px;
+    margin-bottom: 40px;
+    gap: 80px;
+  }
+  .chart-item {
+    width: 30%;
+    height: 240px; 
+  }
+}
+
+@media (max-width: 1340px) {
+  .graph-container {
+    flex-direction: column;
+    align-items: center;
+    gap: 120px;
+    margin-top: 40px;
+    margin-bottom: 60px;
+  }
+  .chart-item {
+    width: 30%;
+    height: 200px; 
+  }
+}
+
+@media (max-width: 950px) {
+  .graph-container {
+    flex-direction: column;
+    align-items: center;
+    gap: 170px;
+    margin-top: 70px;
+    margin-bottom: 90px;
+  }
+
+  .chart-item {
+    width: 50%; 
+    height: 150px;
+  }
+}
+
+@media (max-width: 435px) {
+  .graph-container {
+    flex-direction: column;
+    align-items: center;
+    margin-top: 90px;
+    margin-bottom: 120px;
+    gap: 200px;
+  }
+  .chart-item {
+    width: 80%;
+    height: 100px; 
+    margin-bottom: 20px;
+  }
+}
 </style>
