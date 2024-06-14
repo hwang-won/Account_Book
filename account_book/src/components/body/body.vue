126c18317be9ecb416a5b1853782928fb64acacb
JaesungGo <102339979+JaesungGo@users.noreply.github.com>
2024-06-14 09:45:48 +0900

    Update body.vue

--- account_book/src/components/body/body.vue
+++ account_book/src/components/body/body.vue
@@ -1,20 +1,28 @@
 <template>
     <div>
-        <Graph/>
-        <RecentList/>
+      <Graph/>
+      <RecentList ref="recentListComponent" @recent-add-transaction="handleRecentAddTransaction" />
     </div>
-</template>
-
-<script>
-import Graph from "@/components/body/graph.vue"
-import RecentList from "@/components/body/recentList.vue";
-
-export default {
+  </template>
+  
+  <script>
+  import Graph from "@/components/body/graph.vue";
+  import RecentList from "@/components/body/recentList.vue";
+  
+  export default {
     name: "Body",
-    components : {Graph, RecentList}
-}
-</script>
+    components: { Graph, RecentList },
+    methods: {
+      handleRecentAddTransaction(transactionData) {
 
-<style>
-    
-</style>
+      },
+      addTransactionToRecentList(transactionData) {
+        this.$refs.recentListComponent.handleAddTransaction(transactionData);
+      }
+    }
+  };
+  </script>
+  
+  <style scoped>
+  </style>
+  
