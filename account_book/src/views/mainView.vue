c91fe8c0d983ad31df1644cd0f6da788de3919a4
g0mg0m <g0mg0m@pukyong.ac.kr>
2024-06-14 10:22:28 +0900

    feat: CSS notice & noticeDetail

--- account_book/src/views/mainView.vue
+++ account_book/src/views/mainView.vue
@@ -1,18 +1,13 @@
 <template>
-  <div class="main-container">
-    <Header class="header" @toggle_aside="toggle_aside" />
-    <Aside
-      class="aside"
-      v-if="asideOpen"
-      @tab_value="update_body"
-      @tab_home="update_home"
-      v-bind:create_tab="create_tab"
-    />
-    <div class="content" :class="{ 'content-with-aside': asideOpen }">
-      <component :is="tabs" ref="listComponent"></component>
+    <div class="main-container">
+        <Header class="header" @toggle_aside="toggle_aside" />
+        <Aside class="aside" v-if="asideOpen" @tab_value="update_body" @tab_home="update_home"
+            v-bind:create_tab="create_tab" />
+        <div class="content" :class="{ 'content-with-aside': asideOpen }">
+            <component :is="tabs" ref="listComponent"></component>
+        </div>
+        <Toggle class="toggle" @add-transaction="handleAddTransaction" />
     </div>
-    <Toggle class="toggle" @add-transaction="handleAddTransaction" />
-  </div>
 </template>
 
 <script>
@@ -27,89 +22,89 @@ import Toggle from '@/components/body/addToggle.vue';
 import RecentList from '@/components/body/recentList.vue';
 
 export default {
-  name: "MainView",
-  components:{Header, Aside, Body, Client, Detail, List, Notice, Toggle, RecentList},
-  data() {
-    return {
-      tabs: "Body",
-      create_tab: "Body",
-      asideOpen: false,
-    };
-  },
-  methods: {
-    checkloginStatus() {
-      const value = localStorage.getItem("loginKey");
-      if (value) {
-        console.log("LocalStorage에 값이 있습니다:", value);
-        //this.$router.push('/main');
-      } else {
-        console.log("LocalStorage에 값이 없습니다.");
-        this.$router.push("/");
-      }
+    name: "MainView",
+    components: { Header, Aside, Body, Client, Detail, List, Notice, Toggle, RecentList },
+    data() {
+        return {
+            tabs: "Body",
+            create_tab: "Body",
+            asideOpen: false,
+        };
     },
-    // 사이드바에서 탭 클릭시 동적 컴포넌트 기능으로 컴포넌트 변경
-    update_body(e) {
-      this.tabs = e;
-      this.create_tab = this.tabs;
-    },
+    methods: {
+        checkloginStatus() {
+            const value = localStorage.getItem("loginKey");
+            if (value) {
+                console.log("LocalStorage에 값이 있습니다:", value);
+                //this.$router.push('/main');
+            } else {
+                console.log("LocalStorage에 값이 없습니다.");
+                this.$router.push("/");
+            }
+        },
+        // 사이드바에서 탭 클릭시 동적 컴포넌트 기능으로 컴포넌트 변경
+        update_body(e) {
+            this.tabs = e;
+            this.create_tab = this.tabs;
+        },
 
-    update_home(e) {
-      this.tabs = e;
-      this.create_tab = this.tabs;
-    },
+        update_home(e) {
+            this.tabs = e;
+            this.create_tab = this.tabs;
+        },
 
-    toggle_aside() {
-      this.asideOpen = !this.asideOpen;
+        toggle_aside() {
+            this.asideOpen = !this.asideOpen;
+        },
+        handleAddTransaction(transactionData) {
+            if (this.tabs === "List" && this.$refs.listComponent) {
+                this.$refs.listComponent.handleAddTransaction(transactionData);
+            }
+            if (this.$refs.recentListComponent) {
+                this.$refs.recentListComponent.handleAddTransaction(transactionData);
+            }
+        },
     },
-    handleAddTransaction(transactionData) {
-      if (this.tabs === "List" && this.$refs.listComponent) {
-        this.$refs.listComponent.handleAddTransaction(transactionData);
-      }
-      if (this.$refs.recentListComponent) {
-        this.$refs.recentListComponent.handleAddTransaction(transactionData);
-      }
-    },
-  },
 };
 </script>
 <style scoped>
 .header {
-  z-index: 4;
+    z-index: 4;
 }
 
 .main-container {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  margin-top: 0;
-  height: 100vh;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    margin-top: 0;
+    height: 100vh;
 }
 
 .content {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  padding: 20px;
-  box-sizing: border-box;
-  width: 100%; 
-  max-width: 1500px; 
-  margin-left: 0; 
-  transition: margin-left 0.3s ease; 
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    padding: 20px;
+    box-sizing: border-box;
+    width: 100%;
+    max-width: 1500px;
+    margin-left: 0;
+    transition: margin-left 0.3s ease;
 }
 
 .content-with-aside {
-  margin-left: 150px; 
+    margin-left: 150px;
 }
 
 .aside {
-  position: fixed;
-  top: 0;
-  left: 0;
-  z-index: 3;
+    position: fixed;
+    top: 0;
+    left: 0;
+    z-index: 3;
 }
 
 .toggle {
-  position: fixed;
-  z-index: 5;
+    position: fixed;
+    z-index: 5;
 }
 </style>
